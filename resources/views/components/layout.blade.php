459201f77b8ba57767143f35e44ea63cdb7ac123
vlau4 <laukovav4@gmail.com>
2023-11-24 23:28:06 +0100

    procfile

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -14,29 +14,35 @@
 <body class="mb-48">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.1.1/flowbite.min.js"></script>
         {{-- <nav class="flex justify-between items-center mb-4"> --}}
+
+        {{-- LOGO --}}
         <nav class="flex items-center mb-4">
         <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"/></a>
-        <button id="createButton" data-dropdown-toggle="create" data-dropdown-trigger="hover" class="ml-20 text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700" type="button">Create <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-        </svg>
-        </button>
-        
-        <!-- Dropdown menu -->
-        <div id="create" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="createButton">
-                <li class="hover:text-sky-700 px-2">
-                    <a href="{{ Route('event.create') }}" class="hover:text-sky-700"><i class="fa-solid fa-plus"></i> New Event</a>
-                </li>
-                <li class="hover:text-sky-700 px-2">
-                    <a href="{{ Route('category.create') }}" class="hover:text-sky-700"><i class="fa-solid fa-plus"></i> New Category</a>
-                </li>
-                <li class="hover:text-sky-700 px-2">
-                    <a href="{{ Route('location.create') }}" class="hover:text-sky-700"><i class="fa-solid fa-plus"></i> New Location</a>
-                </li>
-            </ul>
-        </div>
+
         @auth
             @if(auth()->user()->role == 'manager' || auth()->user()->role == 'admin')
+                {{-- CREATE --}}
+            <button id="createButton" data-dropdown-toggle="create" data-dropdown-trigger="hover" class="ml-20 text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700" type="button">Create <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                </svg>
+                </button>
+                
+                <!-- Dropdown menu -->
+                <div id="create" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="createButton">
+                        <li class="px-2">
+                            <a href="{{ Route('event.create') }}" class=""><i class="fa-solid fa-plus"></i> New Event</a>
+                        </li>
+                        <li class="px-2">
+                            <a href="{{ Route('category.create') }}" class=""><i class="fa-solid fa-plus"></i> New Category</a>
+                        </li>
+                        <li class="px-2">
+                            <a href="{{ Route('location.create') }}" class=""><i class="fa-solid fa-plus"></i> New Location</a>
+                        </li>
+                    </ul>
+                </div>
+
+                {{-- CONFIRM --}}
                 <button id="confirmationButton" data-dropdown-toggle="confirmation" data-dropdown-trigger="hover" class="ml-20 text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700" type="button">Confirmation <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                 </svg>
@@ -45,18 +51,19 @@
                 <!-- Dropdown menu -->
                 <div id="confirmation" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                     <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="confirmationButton">
-                        <li class="hover:text-sky-700 px-2">
-                            <a href="{{ Route('event.confirm.show') }}" class="hover:text-sky-700"><i class="fa-solid fa-check"></i> Confirm Events</a>
+                        <li class="px-2">
+                            <a href="{{ Route('event.confirm.show') }}" class=""><i class="fa-solid fa-check"></i> Confirm Events</a>
                         </li>
-                        <li class="hover:text-sky-700 px-2">
-                            <a href="{{ Route('category.confirm.show') }}" class="hover:text-sky-700"><i class="fa-solid fa-check"></i> Confirm Categories</a>
+                        <li class="px-2">
+                            <a href="{{ Route('category.confirm.show') }}" class=""><i class="fa-solid fa-check"></i> Confirm Categories</a>
                         </li>
-                        <li class="hover:text-sky-700 px-2">
-                            <a href="{{ Route('location.confirm.show') }}" class="hover:text-sky-700"><i class="fa-solid fa-check"></i> Confirm Locations</a>
+                        <li class="px-2">
+                            <a href="{{ Route('location.confirm.show') }}" class=""><i class="fa-solid fa-check"></i> Confirm Locations</a>
                         </li>
                     </ul>
                 </div>
 
+                {{-- MANAGE --}}
                 <button id="manageButton" data-dropdown-toggle="manage" data-dropdown-trigger="hover" class="ml-20 text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700" type="button">Manage Categories <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                 </svg>
@@ -65,17 +72,31 @@
                 <!-- Dropdown menu -->
                 <div id="manage" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                     <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="manageButton">
-                        <li class="hover:text-sky-700 px-2">
-                            <a href="{{ Route('categories.manage') }}" class="hover:text-sky-700"><i class="fa-solid fa-check"></i> Manage Categories</a>
+                        <li class="px-2">
+                            <a href="{{ Route('categories.manage') }}" class=""><i class="fa-solid fa-check"></i> Manage Categories</a>
                         </li>
-                        <li class="hover:text-sky-700 px-2">
-                            <a href="{{ Route('locations.manage') }}" class="hover:text-sky-700"><i class="fa-solid fa-check"></i> Manage Locations</a>
+                        <li class="px-2">
+                            <a href="{{ Route('locations.manage') }}" class=""><i class="fa-solid fa-check"></i> Manage Locations</a>
                         </li>
                     </ul>
                 </div>
+            @else
+                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" >
+                    <li class="ml-20 text-white bg-sky-900 hover:bg-sky-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+                        <a href="{{ Route('event.create') }}" class=""><i class="fa-solid fa-plus"></i> Create Event</a>
+                    </li>
+                    <li class="ml-20 text-white bg-sky-900 hover:bg-sky-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+                        <a href="{{ Route('category.create') }}" class=""><i class="fa-solid fa-plus"></i> Create Category</a>
+                    </li>
+                    <li class="ml-20 text-white bg-sky-900 hover:bg-sky-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+                        <a href="{{ Route('location.create') }}" class=""><i class="fa-solid fa-plus"></i> Create Location</a>
+                    </li>
+                </ul>
             @endif
 
             @if(auth()->user()->role == 'admin')
+
+                {{-- MANAGE USERS --}}
                 <ul>
                     <li class="ml-20 text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
                         <a href="{{ Route('users') }}" class=""><i class="fa-solid fa-gear"></i> Manage Users</a>
@@ -83,35 +104,41 @@
                 </ul>
             @endif
 
+            @if(auth()->user()->role == 'manager' || auth()->user()->role == 'admin')
+            {{-- USER --}}
             <button id="userButton" data-dropdown-toggle="user" data-dropdown-trigger="hover" class="ml-20 text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700" type="button">{{auth()->user()->name}} <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
             </svg>
             </button>
             
-            <!-- Dropdown menu -->
-            <div id="user" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="userButton">
-                    <li class="hover:text-sky-700 px-2">
-                        <a href="{{ Route('events.mine') }}" class="hover:text-sky-700"><i class="fa-solid fa-calendar-days"></i> My Events</a>
-                    </li>
-                    <li class="hover:text-sky-700 px-2">
-                        <a href="{{ Route('events.manage') }}" class="hover:text-sky-700"><i class="fa-solid fa-gear"></i> Manage Events</a>
-                    </li>
-                    <li class="hover:text-sky-700 px-2">
-                        <a href="{{ Route('settings') }}" class="hover:text-sky-700"><i class="fa-solid fa-gear"></i> Settings</a>
-                    </li>
-                    <li class="hover:text-sky-700 px-2">
-                        <form method="POST" action="/logout" class="inline">
-                            @csrf
-                            <button type="submit">
-                                <i class="fa-solid fa-door-open"></i> Logout
-                            </button>
-                        </form>
-                    </li>
-                </ul>
-            </div>
+                <!-- Dropdown menu -->
+                <div id="user" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="userButton">
+                        <li class="px-2">
+                            <a href="{{ Route('events.mine') }}" class=""><i class="fa-solid fa-calendar-days"></i> My Events</a>
+                        </li>
+                        <li class="px-2">
+                            <a href="{{ Route('events.manage') }}" class=""><i class="fa-solid fa-gear"></i> Manage Events</a>
+                        </li>
+                        <li class="px-2">
+                            <a href="{{ Route('settings') }}" class=""><i class="fa-solid fa-gear"></i> Settings</a>
+                        </li>
+                        <li class="px-2">
+                            <form method="POST" action="/logout" class="inline">
+                                @csrf
+                                <button type="submit">
+                                    <i class="fa-solid fa-door-open"></i> Logout
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            @else
+
             
         @else
+
+        {{-- UNAUTH - LOGIN, REGISTER --}}
         <ul>
             <li class="ml-20 text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
                 <a href="/register" class=""><i class="fa-solid fa-user-plus"></i> Register</a>
