a24e3935599267908a22e2ca4c1d71b3b5d5fc08
vlau4 <laukovav4@gmail.com>
2023-11-25 15:27:39 +0100

    server

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => $JAWSDB_URL,
-            'host' => $DB_HOST,
-            'port' => $DB_PORT,
-            'database' => $DB_DATABASE,
-            'username' => $DB_USERNAME,
-            'password' => $DB_PASSWORD,
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
