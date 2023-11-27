107ce0986ae9b84ef9133b3d634154081eb61202
vlau4 <laukovav4@gmail.com>
2023-11-27 01:12:25 +0100

    padding

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,7 +22,7 @@
         @auth
             @if(auth()->user()->role == 'manager' || auth()->user()->role == 'admin')
                 {{-- CREATE --}}
-            <button id="createButton" data-dropdown-toggle="create" data-dropdown-trigger="hover" class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700" type="button">Create <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+            <button id="createButton" data-dropdown-toggle="create" data-dropdown-trigger="hover" class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700" type="button">Create <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                 </svg>
                 </button>
@@ -44,14 +44,14 @@
 
                 {{-- CONFIRM --}}
                 <ul>
-                    <li class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+                    <li class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
                         <a href="{{ Route('confirm') }}" class=""><i class="fa-solid fa-gear"></i> Confirm</a>
                     </li>
                 </ul>
 
                 @if(auth()->user()->role == 'admin')
                     {{-- MANAGE --}}
-                    <button id="manageButton" data-dropdown-toggle="manage" data-dropdown-trigger="hover" class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700" type="button">Manage<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <button id="manageButton" data-dropdown-toggle="manage" data-dropdown-trigger="hover" class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700" type="button">Manage<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                     </svg>
                     </button>
@@ -70,23 +70,23 @@
                     </div>
                 @else
                     <ul class="py-2  text-gray-700 dark:text-gray-200" aria-labelledby="manageButton">
-                        <li class="text-white bg-sky-900 hover:bg-sky-700 font-medium rounded-lg  px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+                        <li class="text-white bg-sky-900 hover:bg-sky-700 font-medium rounded-lg  px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
                             <a href="{{ Route('categories.manage') }}" class="hover:text-sky-700"><i class="fa-solid fa-check"></i> Manage Categories</a>
                         </li>
-                        <li class="text-white bg-sky-900 hover:bg-sky-700 font-medium rounded-lg  px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+                        <li class="text-white bg-sky-900 hover:bg-sky-700 font-medium rounded-lg  px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
                             <a href="{{ Route('locations.manage') }}" class="hover:text-sky-700"><i class="fa-solid fa-check"></i> Manage Locations</a>
                         </li>
                     </ul>
                 @endif
             @else
                 <ul class="py-2  text-gray-700 dark:text-gray-200" >
-                    <li class="text-white bg-sky-900 hover:bg-sky-700 font-medium rounded-lg  px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+                    <li class="text-white bg-sky-900 hover:bg-sky-700 font-medium rounded-lg  px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
                         <a href="{{ Route('event.create') }}" class=""><i class="fa-solid fa-plus"></i> Create Event</a>
                     </li>
-                    <li class="text-white bg-sky-900 hover:bg-sky-700 font-medium rounded-lg  px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+                    <li class="text-white bg-sky-900 hover:bg-sky-700 font-medium rounded-lg  px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
                         <a href="{{ Route('category.create') }}" class=""><i class="fa-solid fa-plus"></i> Create Category</a>
                     </li>
-                    <li class=" text-white bg-sky-900 hover:bg-sky-700 font-medium rounded-lg  px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+                    <li class=" text-white bg-sky-900 hover:bg-sky-700 font-medium rounded-lg  px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
                         <a href="{{ Route('location.create') }}" class=""><i class="fa-solid fa-plus"></i> Create Location</a>
                     </li>
                 </ul>
@@ -96,7 +96,7 @@
 
                 {{-- MANAGE USERS --}}
                 <ul>
-                    <li class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+                    <li class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
                         <a href="{{ Route('users') }}" class=""><i class="fa-solid fa-gear"></i> Manage Users</a>
                     </li>
                 </ul>
@@ -104,7 +104,7 @@
 
             @if(auth()->user()->role == 'manager' || auth()->user()->role == 'admin')
                 {{-- USER --}}
-                <button id="userButton" data-dropdown-toggle="user" data-dropdown-trigger="hover" class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700" type="button">{{auth()->user()->name}} <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <button id="userButton" data-dropdown-toggle="user" data-dropdown-trigger="hover" class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700" type="button">{{auth()->user()->name}} <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                 </svg>
                 </button>
@@ -133,16 +133,16 @@
                 </div>
             @else
                 <ul>
-                    <li class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+                    <li class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
                         <a href="{{ Route('events.mine') }}" class=""><i class="fa-solid fa-calendar-days"></i> My Events</a>
                     </li>
-                    <li class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+                    <li class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
                         <a href="{{ Route('events.manage') }}" class=""><i class="fa-solid fa-gear"></i> Manage Events</a>
                     </li>
                 </ul>
 
                 {{-- USER --}}
-                <button id="userButton" data-dropdown-toggle="user" data-dropdown-trigger="hover" class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700" type="button">{{auth()->user()->name}} <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <button id="userButton" data-dropdown-toggle="user" data-dropdown-trigger="hover" class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700" type="button">{{auth()->user()->name}} <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                 </svg>
                 </button>
@@ -170,10 +170,10 @@
 
         {{-- UNAUTH - LOGIN, REGISTER --}}
         <ul>
-            <li class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+            <li class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
                 <a href="/register" class=""><i class="fa-solid fa-user-plus"></i> Register</a>
             </li>
-            <li class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-8 mx-8 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
+            <li class=" text-white bg-sky-900 hover:bg-sky-700 focus:outline-none font-medium rounded-lg  px-6 mx-6 py-2.5 text-center inline-flex items-center dark:bg-sky-900 dark:hover:bg-sky-700">
                 <a href="/login" class=""><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
             </li>
         </ul>
@@ -186,7 +186,7 @@
     <footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-sky-900 text-white h-24 mt-24 opacity-90 md:justify-center">
         <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
 
-        <a href="{{ Route('event.create') }}" class="absolute top-1/3 right-10 bg-black text-white py-2 px-8 mx-8">Post Event</a>
+        <a href="{{ Route('event.create') }}" class="absolute top-1/3 right-10 bg-black text-white py-2 px-6 mx-6">Post Event</a>
     </footer>
     <x-flash-message></x-flash-message>
     <x-flash-error></x-flash-error>
