fb0cae4cb9dcfb57c9e8288f712410cd353695fb
vlau4 <laukovav4@gmail.com>
2023-11-27 11:24:25 +0100

    opacity

--- resources/views/components/flash-error.blade.php
+++ resources/views/components/flash-error.blade.php
@@ -1,6 +1,6 @@
 @if(session()->has('err'))
     <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)"
-        x-show='show' class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-rose-800 text-white px-48 py-3">
+        x-show='show' class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-rose-800 text-white px-48 py-3 opacity-90">
         <p>
             {{session('err')}}
         </p>
