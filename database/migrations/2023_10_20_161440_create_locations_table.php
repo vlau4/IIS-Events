8bc2cf6fd2bc547a908fcb790b193731888502e9
vlau4 <laukovav4@gmail.com>
2023-11-27 13:03:14 +0100

    number string

--- database/migrations/2023_10_20_161440_create_locations_table.php
+++ database/migrations/2023_10_20_161440_create_locations_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
             $table->string('street');
-            $table->smallInteger('number');
+            $table->string('number');
             $table->string('city');
             $table->integer('zip');
             $table->string('country');
